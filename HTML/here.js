d26c4fa7d7032362a1fefbdd72a7e4ce9ef968bf
Will Hughes <41086361+willza3@users.noreply.github.com>
2019-02-08 22:45:07 +0000

    Added map container
    Map isn't really working properly as of now

--- HTML/here.js
+++ HTML/here.js
@@ -3,3 +3,15 @@ var platform = new H.service.Platform({
   'app_id': '{FOS7x9AxfWAnpKpjTGnV}',
   'app_code': '{Ypbj9r3Koexac-WWPBbe_A}'
 });
+
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  defaultLayers.normal.map,
+  {
+    zoom: 10,
+    center: { lat: 53.2274, lng: 4.1293 }
+  });
