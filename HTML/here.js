538b574e1f14bbf5ffba2056c467723e588966e9
Will Hughes <41086361+willza3@users.noreply.github.com>
2019-02-08 18:00:39 +0000

    Added example HTML and more backend stuff

---
+++ HTML/here.js
@@ -0,0 +1,5 @@
+// Initialises communication with HERE's back-end services //
+var platform = new H.service.Platform({
+  'app_id': '{FOS7x9AxfWAnpKpjTGnV}',
+  'app_code': '{Ypbj9r3Koexac-WWPBbe_A}'
+});
